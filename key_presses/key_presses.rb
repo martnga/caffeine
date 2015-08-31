ed523d9b5797e85e23258a2da7993b310e7b252a
iantheninja <munene.ian@live.com>
2015-08-31 09:43:16 +0300

    debugged solution to keypresses

--- key_presses/key_presses.rb
+++ key_presses/key_presses.rb
@@ -22,18 +22,22 @@
 # ------- ------- -------
 
 
-# Before predictive text entry systems like T9, you had to press a button repeatedly to cycle through the possible values until you reached the one you wanted. 
-# For example, to type "V8" you would press the 8 key three times and then again four times (pressing the 8 key cycles through T->U->V->8), giving us a total of seven key presses. 
+# Before predictive text entry systems like T9, you had to press a button repeatedly to cycle through the possible values
+# until you reached the one you wanted.
+# For example, to type "V8" you would press the 8 key three times and then again four times
+# (pressing the 8 key cycles through T->U->V->8), giving us a total of seven key presses.
 
 # Note: the 0 key handles spaces and outputs 0 when tapped twice.
 
-# Write a function that can calculate the amount of button presses required for any phrase. Except for spaces, punctuation can be ignored. Your function should accept both uppercase and lowercase letters and treat them the same.
+# Write a function that can calculate the amount of button presses required for any phrase.
+# Except for spaces, punctuation can be ignored. Your function should accept both uppercase and lowercase
+# letters and treat them the same.
 
 # Examples:
 
 # presses('V8') # 7
 # presses('LOL') # 9
-# presses('How R u 2day') # 23 
+# presses('How R u 2day') # 23
 # presses("i 8 2 Many mandazi 4 brekky") # 55
 
 # Bonus: Try to avoid hard-coding the number of button presses for each letter!
@@ -43,18 +47,21 @@ def presses(str)
   # Your Code Here!
   num_press = 0
   str.downcase.each_char do |letter|
-  	if letter == " 1adgjmptw*#".index(index)
+  	if " 1adgjmptw*#".include? letter
   		num_press += 1
-  	elsif "0behknqux".index(letter)
+  	elsif "0behknqux".include? letter
   		num_press += 2
-  	elsif "234568sz".index(letter)
-  		num_press += 3
-  	elsif "cfilorvy".index(letter)
-  		num_press += 2
-  	elsif "79".index(letter)
+  	elsif "234568sz".include? letter
   		num_press += 4
-  	elsif "0behknqux".index(letter)
-  		num_press += 2	
+  	elsif "cfilorvy".include? letter
+  		num_press += 3
+  	elsif "79".include? letter
+  		num_press += 5
+  	elsif "0behknqux".include? letter
+  		num_press += 2
   	end
+end
   	return num_press
 end
+
+p presses(' ')
